95c73da7465e3aa89d6e28cbf66047b7f213aa26
TechZi <zhangyuyi@15826674D2279F9.internal.baidu.com>
2013-07-22 19:31:51 +0800

    Cloud Storage Interface and QiNiu Storage

---
+++ application/library/Conf/QiNiu.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Conf_QiNiu {
+	const ACCESS_KEY = '-ADyWaRMa2fL81ZQ6H0y1X5HjZiDtvO80H50fUxd';	
+	const SECRET_KEY = 'QLFBWMDhoTbfRlJVHEM8zqOCZa8fLa0dAY4KWWtn';
+
+	const BUCKET = 'day365i';		
+}
+?>
