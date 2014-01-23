b28f19db874cca74951fe321544d3cb713558b6d
zhangyuyi <yeszyy@gmail.com>
2014-01-23 16:12:39 +0800

    完成关注人列表页

--- application/controllers/Follow.php
+++ application/controllers/Follow.php
@@ -2,22 +2,29 @@
 
 class FollowController extends BasicController {
     public function indexAction() {
-        $pageNo = $this->getRequiredParam('page');
+        $pageNo = $this->getOptionalParam('page', 1);
+
+        if (!$this->isSelf) {
+            $userId = $this->getOptionalParam('p', 0, true);
+            $userInfo = UserModel::getInstance()->getUserInfoById($userId);
+        } else {
+            $userId = $_SESSION['user_id'];
+            $userInfo = $this->userInfo;
+        }
 
         $offset = ($pageNo - 1) * AttentionModel::DEFAULT_LIMIT;
-        $followUids = AttentionModel::getInstance()->getFollowUids($_SESSION['user_id'], $offset);
+        $followUids = AttentionModel::getInstance()->getFollowUids($userId, $offset);
 
         $follows = array();
         if (!empty($followUids)) {
             $follows = UserModel::getInstance()->getUserInfos($followUids);
         }
 
-        $followNum = AttentionNumModel::getInstance()->getFansNums($followUids);
-
         foreach($follows as &$follow) {
-            $follow['fans_num'] = $followNum[$follow['user_id']];
+            $follow['diary_days'] = DiaryModel::getInstance()->getDiaryAmountByUid($follow['user_id']);
         }
 
-        $this->getView()->assign('follows', $follow);
+        $this->getView()->assign('userInfo', $userInfo);
+        $this->getView()->assign('follows', $follows);
     }
 }
